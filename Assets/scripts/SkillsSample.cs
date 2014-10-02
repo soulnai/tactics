333751945d66615c5a879b5df1d2075f8048840a
Chewie <vasiliev.art@gmail.com>
2014-10-02 16:19:08 +0300

    Skills sample
    пример массива енумов

---
+++ Assets/scripts/SkillsSample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillsSample : MonoBehaviour {
+
+	public enum skills{
+		iceArrow,
+		fireball,
+		stun
+	}
+	public skills[] skillsList;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
