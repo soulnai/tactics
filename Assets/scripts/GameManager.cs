cb854f025bf214657e6cdf2c60f0a5400ecc11c8
Chewie <vasiliev.art@gmail.com>
2014-10-11 13:14:22 +0300

    battle log fixes

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -215,7 +215,7 @@ public class GameManager : MonoBehaviour {
 						int amountOfDamage = (int)Mathf.Floor(units[currentUnitIndex].damageBase + Random.Range(0, units[currentUnitIndex].damageRollSides));
 
 						target.takeDamage(amountOfDamage);
-							GUImanager.instance.battleLog.text += units[currentUnitIndex].unitName + " <b>successfuly hit by the sword " + target.unitName + " for <b>" + amountOfDamage + " damage</b>!\n";
+							GUImanager.instance.battleLog.text += units[currentUnitIndex].unitName + " successfuly hit by the sword " + target.unitName + " for <b><color=red>" + amountOfDamage + " damage</color></b>!\n";
 						Debug.Log(units[currentUnitIndex].unitName + " successfuly hit " + target.unitName + " for " + amountOfDamage + " damage!");
 					} else {
 							GUImanager.instance.battleLog.text += units[currentUnitIndex].unitName + " missed " + target.unitName + "!\n";
@@ -278,7 +278,7 @@ public class GameManager : MonoBehaviour {
 								int amountOfDamage = (int)Mathf.Floor(units[currentUnitIndex].damageBase * (units[currentUnitIndex].Magic/100) + Random.Range(0, units[currentUnitIndex].damageRollSides));
 
 						target.takeDamage(amountOfDamage);
-								GUImanager.instance.battleLog.text += units[currentUnitIndex].unitName + " <b>successfuly hit " + target.unitName + " for <b>" + amountOfDamage + " damage</b>!\n";
+								GUImanager.instance.battleLog.text += units[currentUnitIndex].unitName + " successfuly hit " + target.unitName + " for <b><color=red>" + amountOfDamage + " damage</color></b>!\n";
 						Debug.Log(units[currentUnitIndex].unitName + " successfuly hit " + target.unitName + " for " + amountOfDamage + " damage!");
 					} else {
 						GUImanager.instance.battleLog.text += units[currentUnitIndex].unitName + " missed " + target.unitName + "!\n";
