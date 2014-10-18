e7e9bf6285954cbd087ec6c1a96ee7663e6346ae
Chewie <vasiliev.art@gmail.com>
2014-10-19 01:19:23 +0300

    minor fixes

--- Assets/scripts/skills/BaseAbility.cs
+++ Assets/scripts/skills/BaseAbility.cs
@@ -18,6 +18,8 @@ public class BaseAbility : ICloneable{
 
 	//target flags
 	public bool requireTarget = true;
+	//TODO add check in ability logic
+	public bool enemieUse = false;
 	public bool allyUse = false;
 	public bool selfUse = false;
 
